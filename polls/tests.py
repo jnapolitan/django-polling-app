34eced5174eb3394726c1de19561c10e57160ce5
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-08 18:12:47 -0800

    Write tests for Detail view

--- polls/tests.py
+++ polls/tests.py
@@ -52,7 +52,7 @@ class QuestionIndexViewTests(TestCase):
     self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
 
   def test_future_question(self):
-    create_question(question_text="Future question", days=30)
+    create_question(question_text="Future question.", days=30)
     response = self.client.get(reverse('polls:index'))
     self.assertContains(response, "No polls are available.")
     self.assertQuerysetEqual(response.context['latest_question_list'], [])
@@ -68,3 +68,16 @@ class QuestionIndexViewTests(TestCase):
     create_question(question_text="Past question 2.", days=-5)
     response = self.client.get(reverse('polls:index'))
     self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 2.>', '<Question: Past question 1.>'])
+
+class QuestionDetailViewTests(TestCase):
+  def test_future_question(self):
+    future_question = create_question(question_text="Future question.", days=5)
+    url = reverse('polls:detail', args=(future_question.id,))
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
+
+  def test_past_question(self):
+    past_question = create_question(question_text="Past question.", days=-5)
+    url = reverse('polls:detail', args=(past_question.id,))
+    response = self.client.get(url)
+    self.assertContains(response, past_question.question_text)
